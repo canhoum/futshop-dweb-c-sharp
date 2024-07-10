fbf0bc8fc8f5dbad04908567bf12a747aa53779f
aluno22116 <aluno22116@ipt.pt>
2024-07-10 20:47:28 +0100

    criado o APIController para conectar ao react
    criado o APIController para conectar ao react

---
+++ futshop-dweb/Controllers/APIContoller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace futshop_dweb.Controllers
+{
+
+    [ApiController]
+    [Route("api/madeira")]
+    public class APIContoller : Controller
+
+
+    {
+
+        [Route("login")]
+        public IActionResult Login()
+        {
+            //Obtem utilizadores
+            //Verifica password
+            //Retorna OK
+        }
+
+
+
+
+    }
+}
